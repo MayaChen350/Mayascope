ebdc1d7023ccf4078dad712da6cad06e459cd143
Maya <145165822+MayaChen350@users.noreply.github.com>
2026-02-23 21:29:40 -0500

    ICON! (+ upgraded dependencies)
    Co-authored-by: MasterFoann <86154343+MasterFoann@users.noreply.github.com>

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -23,7 +23,7 @@ kotlin {
 
     sourceSets {
         androidMain.dependencies {
-            implementation(compose.preview)
+            implementation(libs.ui.tooling.preview)
             implementation(libs.androidx.activity.compose)
             implementation(libs.androidx.ui)
             implementation(libs.androidx.datastore.preferences)
@@ -37,12 +37,12 @@ kotlin {
 //            androidTestImplementation(libs.androidx.ui.test.junit4)
 //        }
         commonMain.dependencies {
-            implementation(compose.runtime)
-            implementation(compose.foundation)
-            implementation(compose.material3)
-            implementation(compose.ui)
-            implementation(compose.components.resources)
-            implementation(compose.components.uiToolingPreview)
+            implementation(libs.runtime)
+            implementation(libs.foundation)
+            implementation(libs.material3)
+            implementation(libs.ui)
+            implementation(libs.components.resources)
+            implementation(libs.ui.tooling.preview)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
             implementation(libs.kotlinx.datetime)
@@ -61,8 +61,8 @@ android {
         applicationId = "io.github.mayachen350.mayascope"
         minSdk = 26
         targetSdk = 34
-        versionCode = 5
-        versionName = "2025.12.1"
+        versionCode = 6
+        versionName = "2026.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
--- composeApp/src/commonMain/kotlin/io/github/mayachen350/mayascope/App.kt
+++ composeApp/src/commonMain/kotlin/io/github/mayachen350/mayascope/App.kt
@@ -4,10 +4,8 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import io.github.mayachen350.mayascope.ui.pages.HomePageHaver
 import io.github.mayachen350.mayascope.ui.theme.MayascopeTheme
-import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
-@Preview
 fun App() {
     MayascopeTheme {
         Surface {
